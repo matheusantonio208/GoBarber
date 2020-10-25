ab95cbd9f37c2dc82164fcdff4adbd3fde08cb89
Matheus Antonio <matheus.antonio208@gmail.com>
2020-10-24 22:46:35 -0300

    add routes-api file

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   port: '5432',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
